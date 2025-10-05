6e1c4ed73d64492720db098f063addbc9fa15d18
EM <greyhatem@gmail.com>
2025-10-05 12:38:34 +0530

    Version 1

---
+++ lib/create_ui.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+import 'package:attender/app_data.dart';
+import 'package:attender/action_buttons.dart';
+
+class ShowCards extends StatefulWidget {
+  const ShowCards({super.key});
+
+  @override
+  State<ShowCards> createState() => _ShowCards();
+}
+
+class _ShowCards extends State<ShowCards> {
+  final Data data = Data();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        spacing: 10.0,
+        children: data.getCurrentWorkspace().outputCards(context),
+      ),
+    );
+  }
+}
+
+class ShowActionButtons extends StatefulWidget {
+  const ShowActionButtons({super.key});
+
+  @override
+  State<ShowActionButtons> createState() => _ShowActionButtons();
+}
+
+class _ShowActionButtons extends State<ShowActionButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 5.0,
+      children: [
+        Expanded(
+          child: ElevatedButton.icon(
+          icon: const Icon(Icons.menu_open),
+          label: Text("Open"),
+          onPressed: () {
+            showActionSheet(context);
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(5.0)
+            )
+          ),
+         ),
+        ),
+        Expanded(
+          child: ElevatedButton.icon(
+            icon: const Icon(Icons.save),
+            label: Text("Save"),
+            onPressed: () {
+              jsonizeAndWrite("dateFile");
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(5.0)
+              )
+            ),
+          ),
+        ),
+        Expanded(
+          child: ElevatedButton.icon(
+            icon: const Icon(Icons.copy),
+            label: Text("Copy"),
+            onPressed: () {
+              copyToClipboard();
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(5.0)
+              )
+            ),
+           ),
+        ),
+      ],
+    );
+  }
+}
+
+class ShowDrawer extends StatefulWidget {
+  const ShowDrawer({super.key});
+
+  @override
+  State<ShowDrawer> createState() => _ShowDrawer();
+}
+
+class _ShowDrawer extends State<ShowDrawer> {
+  final TextStyle textStyle = TextStyle(
+    fontFamily: "Monospace",
+  );
+  final Data data = Data();
+  final TextEditingController _textController = TextEditingController();
+  final TextEditingController _alwaysPresentController = TextEditingController();
+  final TextEditingController _sometimesPresentController = TextEditingController();
+  final TextEditingController _alwaysAbsentController = TextEditingController();
+  final TextEditingController _outputFormatter = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Data().setDrawerUpdate(drawerUpdate);
+  }
+
+  void drawerUpdate() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _textController.text = data.getCurrentWorkspace().title;
+    _alwaysPresentController.text = data.getCurrentWorkspace().alwaysPresent.join(', ');
+    _sometimesPresentController.text = data.getCurrentWorkspace().sometimesPresent.join();
+    _alwaysAbsentController.text = data.getCurrentWorkspace().alwaysAbsent.join();
+    _outputFormatter.text = data.getCurrentWorkspace().outputFormatter;
+
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Drawer(
+      child: Container(
+        width: screenWidth*0.80,
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: Padding(padding: EdgeInsetsGeometry.all(10.0),
+          child: ListView(
+            children: [
+              Text(
+                "Workspace Title",
+                style: textStyle,
+              ),
+              TextField(
+                controller: _textController,
+                onSubmitted: (value) {
+                  data.getCurrentWorkspace().title = value;
+                  data.appUpdate();
+                },
+              ),
+              SizedBox(height: 10,),
+              Text(
+                "Always Present",
+                style: textStyle
+              ),
+              TextField(
+                controller: _alwaysPresentController,
+                onSubmitted: (value) {
+                  setState(() {
+                    data.getCurrentWorkspace().alwaysPresent = data.getCurrentWorkspace().saveAddSubtractRules(value, "aP");
+                  });
+                },
+              ),
+              if (data.getCurrentWorkspace().errFrom=="aP" && data.getCurrentWorkspace().errVal.$1>0)
+                Text(
+                  "Error in Always Present,\nColumn ${data.getCurrentWorkspace().errVal.$1} in Rule '${data.getCurrentWorkspace().errRule}': ${data.getCurrentWorkspace().errStatementArr[data.getCurrentWorkspace().errVal.$2]}",
+                  style: TextStyle(
+                    color: Colors.red
+                  ),
+                ),
+              Text(
+                "Sometimes Present",
+                style: textStyle
+              ),
+              TextField(
+                controller: _sometimesPresentController,
+                onSubmitted: (value) {
+                  setState(() {
+                    data.getCurrentWorkspace().sometimesPresent = data.getCurrentWorkspace().saveAddSubtractRules(value, "sP");
+                  });
+                },
+              ),
+              if (data.getCurrentWorkspace().errFrom=="sP" && data.getCurrentWorkspace().errVal.$1>0)
+                Text(
+                  "Error in Always Present,\nColumn ${data.getCurrentWorkspace().errVal.$1} in Rule '${data.getCurrentWorkspace().errRule}': ${data.getCurrentWorkspace().errStatementArr[data.getCurrentWorkspace().errVal.$2]}",
+                  style: TextStyle(
+                    color: Colors.red
+                  ),
+                ),
+              Text(
+                "Always Absent",
+                style: textStyle
+              ),
+              TextField(
+                controller: _alwaysAbsentController,
+                onSubmitted: (value) {
+                  setState(() {
+                    data.getCurrentWorkspace().alwaysAbsent = data.getCurrentWorkspace().saveAddSubtractRules(value, "aA");
+                  });
+                },
+              ),
+              if (data.getCurrentWorkspace().errFrom=="aA" && data.getCurrentWorkspace().errVal.$1>0)
+                Text(
+                  "Error in Always Present,\nColumn ${data.getCurrentWorkspace().errVal.$1} in Rule '${data.getCurrentWorkspace().errRule}': ${data.getCurrentWorkspace().errStatementArr[data.getCurrentWorkspace().errVal.$2]}",
+                  style: TextStyle(
+                    color: Colors.red
+                  ),
+                ),
+              Text(
+                "Rules Formatter",
+                style: textStyle,
+              ),
+              RulesFormatter(),
+              Text(
+                "Ouptut Formatter",
+                style: textStyle
+              ),
+              TextField(
+                controller: _outputFormatter,
+                textInputAction: TextInputAction.newline,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                onSubmitted: (value) {
+                  data.getCurrentWorkspace().title = value;
+                  data.appUpdate();
+                },
+              ),
+              if (data.getCurrentWorkspace().errFrom=="oF" && data.getCurrentWorkspace().errVal.$1>0)
+                Text(
+                  "Error in Output Format,\nColumn ${data.getCurrentWorkspace().errVal.$1} in Rule '${data.getCurrentWorkspace().errRule}': ${data.getCurrentWorkspace().errStatementArr[data.getCurrentWorkspace().errVal.$2]}"
+                ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton.icon(
+                  onPressed: () {
+                    data.getCurrentWorkspace().saveOutputFormat(_outputFormatter.text, "oF");
+                  },
+                  label: Text("Verify and Add Format"),
+                  icon: const Icon(Icons.add),
+                  style: TextButton.styleFrom(
+                    side: BorderSide(
+                      width: 2.0,
+                      color: Theme.of(context).colorScheme.primary
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)
+                    )
+                  ),
+                )
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Builder(
+                  builder: (ctx) {
+                    return TextButton.icon(
+                      onPressed: () {
+                        data.deleteCurrentWorksapce();
+                        if (data.isDeleted) {
+                          data.appUpdate();
+                          Scaffold.of(context).closeDrawer();
+                        }
+                      },
+                      label: Text("Tap 3 times to Delete Workspace"),
+                      icon: const Icon(Icons.delete),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.red,
+                        side: BorderSide(
+                          width: 2.0,
+                          color: Colors.red
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)
+                        )
+                      ),
+                    );
+                  }
+                )
+              ),
+            ]
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class RulesFormatter extends StatefulWidget {
+  const RulesFormatter({super.key});
+
+  @override
+  State<RulesFormatter> createState() => _RulesFormatter();
+}
+
+class _RulesFormatter extends State<RulesFormatter> {
+  @override
+  Widget build(BuildContext context) {
+    final Data data = Data();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+        children: data.getCurrentWorkspace().buildRulesUI(context)
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:attender/create_ui.dart';
+import 'package:attender/app_data.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await readFile("dataFile");
+  await readFile("dateFile");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Attender',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Attender'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Data data = Data();
+
+  void updateCallback() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    data.setAppUpdate(updateCallback);
+
+    return Scaffold(
+      drawer: ShowDrawer(),
+      body: Column(
+        children: [
+          Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            height: 120.0,
+            child:  Column(
+              children: [
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    if (data.workspaces.isNotEmpty)
+                      Builder(
+                        builder: (context) {
+                          return IconButton(
+                            icon: const Icon(Icons.settings),
+                            onPressed: () {
+                              Scaffold.of(context).openDrawer();
+                            },
+                          );
+                        }
+                      ),
+                    IconButton(
+                      icon: const Icon(Icons.dark_mode),
+                      onPressed: () {},
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "Attender",
+                          style: TextStyle(
+                            fontFamily: "Monsospace",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        )
+                      )
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        data.addNewWorkspace();
+                      },  
+                    )
+                  ],
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(data.workspaces.length, (index) {
+                      Workspace work = data.workspaces[index];
+                      bool isSelected = work.id == data.currentWorkID;
+                      return Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                              color: (isSelected)?(Theme.of(context).colorScheme.secondary):(Colors.transparent),
+                              width: 3.0
+                            )
+                          )
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            data.changeWorkspace(work.id);
+                          },
+                          child: Text(work.title)
+                        )
+                      );
+                    })
+                  )
+                )
+              ]
+            )
+          ),
+          if (data.currentWorkID>=0)
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(15.0),
+                  child: ShowCards()
+                )
+              )
+            ),
+          if (data.workspaces.isNotEmpty)
+            ShowActionButtons(),
+          SizedBox(height: 40,)
+        ]
+      )
+    );
+  }
+}
